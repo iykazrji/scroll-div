abda66bcee507052b8c3d2c5f33a852ddd828568
iyk-azorji <iykazorji@gmail.com>
2018-03-03 07:12:02 +0100

    Fixed exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './demo/App';
+import ScrollDiv from './scroll-div-component/scroll-div-component';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default ScrollDiv;
